ac46d6295a973ba1a194240cf88007f454f961a4
ReanGD <reangd@yandex.ru>
2017-07-01 23:20:03 +0300

    Движение организмов

--- scene.cpp
+++ scene.cpp
@@ -1,8 +1,35 @@
 #include "scene.h"
 
-
-Generator::Generator()
-  : m_generator(std::random_device()()) {
+void Position::move(Direction direction, uint32_t lineSize, uint32_t maxPos) {
+  switch (direction) {
+  case Direction::Up:
+    if (m_value >= lineSize) {
+      m_value -= lineSize;
+    }
+    break;
+  case Direction::Right:
+    if (++m_value % lineSize == 0) {
+      m_value -= lineSize;
+    }
+    break;
+  case Direction::Down:
+    m_value += lineSize;
+    if (m_value >= maxPos) {
+      m_value -= lineSize;
+    }
+    break;
+  case Direction::Left:
+    if (m_value > 0) {
+      if (m_value % lineSize == 0) {
+        m_value += (lineSize - 1);
+      } else {
+        m_value--;
+      }
+    }
+    break;
+  default:
+    break;
+  }
 }
 
 Cell::Cell(CellType type)
@@ -10,15 +37,65 @@ Cell::Cell(CellType type)
 
 }
 
-World::World(uint16_t maxX, uint16_t maxY)
-  : m_maxX(maxX)
-  , m_maxY(maxY) {
+Organism::Organism(Generator& random, Position position)
+  : m_position(position) {
+  for(CommandCode& code: m_genome) {
+    code = random.get(CommandCode::Last);
+  }
+}
 
-  for(uint16_t i=0; i!=m_maxX * m_maxY; ++i) {
-    m_cells.push_back(Cell(m_random.get(CellType::Last)));
+CommandCode Organism::next() {
+  m_commandPos %= m_genome.size();
+  return m_genome[m_commandPos++];
+}
+
+World::World(uint32_t maxX, uint32_t maxY)
+  : m_maxX(maxX)
+  , m_maxY(maxY)
+  , m_cells(maxX * maxY, Cell(CellType::Space)) {
+
+  const size_t botsCount = 100;
+  const uint32_t maxPosition = maxX * maxY;
+  for(size_t i=0; i!=botsCount; ++i) {
+    while(true) {
+      uint32_t pos = m_random.get(maxPosition);
+      if (m_cells[pos].m_type == CellType::Space) {
+        m_organisms.push_front(std::make_unique<Organism>(m_random, Position{pos}));
+        m_cells[pos].m_type = CellType::Organism;
+        break;
+      }
+    }
   }
 }
 
 const std::vector<Cell>& World::step() {
+  const uint32_t maxPos = m_maxX * m_maxY;
+  for (auto& organism: m_organisms) {
+    Position pos = organism->m_position;
+    CommandCode code = organism->next();
+    switch (code) {
+    case CommandCode::MoveUp:
+      pos.move(Direction::Up, m_maxX, maxPos);
+      break;
+    case CommandCode::MoveRight:
+      pos.move(Direction::Right, m_maxX, maxPos);
+      break;
+    case CommandCode::MoveDown:
+      pos.move(Direction::Down, m_maxX, maxPos);
+      break;
+    case CommandCode::MoveLeft:
+      pos.move(Direction::Left, m_maxX, maxPos);
+      break;
+    default:
+      break;
+    }
+    if (m_cells[pos.m_value].m_type == CellType::Space) {
+      m_cells[organism->m_position.m_value].m_type = CellType::Space;
+      m_cells[pos.m_value].m_type = CellType::Organism;
+      organism->m_position = pos;
+    }
+  }
+
   return m_cells;
 }
+
--- scene.h
+++ scene.h
@@ -1,24 +1,31 @@
 #pragma once
 #include <cstdint>
-#include <vector>
 #include <random>
 #include <memory>
+#include <vector>
+#include <forward_list>
 
 
-class Generator {
-public:
-  Generator();
-
+struct Generator {
   // [0; to)
   template<class T> T get(T to) {
     return static_cast<T>(std::uniform_int_distribution<int>(0, static_cast<uint16_t>(to)-1)(m_generator));
   }
 private:
-  std::mt19937 m_generator;
+  std::mt19937 m_generator = std::mt19937(std::random_device()());
+};
+
+enum class Direction : uint8_t {
+  Up,
+  Right,
+  Down,
+  Left,
+  Last
 };
 
 struct Position {
-  uint16_t m_value;
+  void move(Direction direction, uint32_t lineSize, uint32_t maxPos);
+  uint32_t m_value;
 };
 
 //struct Mass {
@@ -43,33 +50,36 @@ struct Cell {
 //  Mass m_mass;
 };
 
+enum class CommandCode : uint8_t {
+  MoveUp,
+  MoveRight,
+  MoveDown,
+  MoveLeft,
+  Last
+};
+
+struct Organism {
+  Organism() = delete;
+  Organism(Generator& random, Position position);
+
+  CommandCode next();
+
+  Position m_position;
+private:
+  std::array<CommandCode, 64> m_genome;
+  uint8_t m_commandPos = 0;
+};
+
 class World {
 public:
   World() = delete;
-  World(uint16_t maxX, uint16_t maxY);
+  World(uint32_t maxX, uint32_t maxY);
 public:
-//  Cell& get(Position pos);
   const std::vector<Cell>& step();
 private:
-  uint16_t m_maxX;
-  uint16_t m_maxY;
+  uint32_t m_maxX;
+  uint32_t m_maxY;
   Generator m_random;
   std::vector<Cell> m_cells;
+  std::forward_list<std::unique_ptr<Organism>> m_organisms;
 };
-
-//enum class CommandType : uint8_t {
-//  MoveUp,
-//  MoveRight,
-//  MoveDown,
-//  MoveLeft,
-//  Last
-//};
-
-//class Organism {
-//public:
-//  Organism();
-
-//  void step(World& world, Cell& current);
-//private:
-//  std::array<CommandType> m_genome;
-//};
